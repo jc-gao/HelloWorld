e3444f7015e6cf0310a20a30a7bd410f0ddaef7b
高健程 <JC@gaojianchengdemacbook-pro.local>
2016-03-14 16:14:38 +0800

    初始化项目

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by 高健程 on 16/3/14.
+//  Copyright © 2016年 hexway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by 高健程 on 16/3/14.
+//  Copyright © 2016年 hexway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
